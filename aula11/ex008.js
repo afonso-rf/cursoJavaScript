e132b71e664b02d056483d872d9ef27356a85e5a
afonso-rf <afonso.rf@gmail.com>
2022-04-20 20:24:18 -0300

    Up

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+// console.log('O console funicionou!')
+
+var vel = 60
+console.log(`A velocidade do seu carro é de ${vel}Km/h.`)
+
+if (vel > 60) { // Condição Simples
+    console.log(`Você ultrapassou a velocidade maxima permitida. MULTADO!`)
+}
+
+console.log('Diriga sempre com o cinto de seguraça. ')
